919ba3c19882b4e9cc9fd437946c5fb2cc08c728
Mackowi <dzierzenga.maciej@gmail.com>
2023-08-17 15:52:37 +0200

    formik and yup intergration, register form validation done

---
+++ frontend/src/schemas/registerSchema.js
@@ -0,0 +1,21 @@
+import * as yup from 'yup'
+
+// const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/
+// min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
+
+export const registerSchema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email('Please enter a valid email').required(),
+  password: yup.string().min(6).required(),
+  // password: yup.string().min(6).matches(passwordRules, { message: "Please create a stronger password" }).required(),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required(),
+})
+
+export const loginSchema = yup.object().shape({
+  email: yup.string().email('Please enter a valid email').required(),
+  password: yup.string().min(6).required(),
+  // password: yup.string().min(6).matches(passwordRules, { message: "Please create a stronger password" }).required(),
+})
--- frontend/src/screens/Register.jsx
+++ frontend/src/screens/Register.jsx
@@ -1,18 +1,18 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import Img from '../assets/images/about3.jpg'
 import Loader from '../components/Loader'
-import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useState } from 'react'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 import { useRegisterMutation } from '../slices/usersApiSlice'
 import { setCredentials } from '../slices/userSlice'
 import { toast } from 'react-toastify'
+import { useFormik } from 'formik'
+import { registerSchema } from '../schemas/registerSchema'
 
 function Register() {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -27,8 +27,7 @@ function Register() {
     }
   }, [navigate, userInfo])
 
-  const submitHandler = async (e) => {
-    e.preventDefault()
+  const submitHandler = async () => {
     if (password !== confirmPassword) {
       return
     } else {
@@ -42,6 +41,27 @@ function Register() {
     }
   }
 
+  const {
+    values,
+    errors,
+    touched,
+    isSubmitting,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: registerSchema,
+    onSubmit: submitHandler,
+  })
+
+  const { name, email, password, confirmPassword } = values
+
   return (
     <Container>
       <Row className='py-5'>
@@ -53,53 +73,71 @@ function Register() {
           className='text-center d-flex flex-column justify-content-center'
         >
           <h3 className='text-primary fw-bold mb-4 mb-md-3 mb-lg-5'>Sign Up</h3>
-          <Form onSubmit={submitHandler}>
+          <Form noValidate onSubmit={handleSubmit} autoComplete='off'>
             <Form.Group controlId='name' className='my-2 my-md-1 '>
               <Form.Label>Name</Form.Label>
               <Form.Control
-                type='name'
+                type='text'
                 placeholder='Enter name'
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value)
-                }}
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.name && !!errors.name}
               ></Form.Control>
+              <Form.Control.Feedback type='invalid'>
+                {errors.name}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId='email' className='my-2 my-md-1 '>
               <Form.Label>Email Address</Form.Label>
               <Form.Control
                 type='email'
                 placeholder='Enter email'
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value)
-                }}
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.email && !!errors.email}
               ></Form.Control>
+              <Form.Control.Feedback type='invalid'>
+                {errors.email}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId='password' className='my-2 my-md-1 '>
               <Form.Label>Password</Form.Label>
-              <Form.Control
-                type='password'
-                placeholder='Enter password'
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value)
-                }}
-              ></Form.Control>
+              <InputGroup>
+                <Form.Control
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Enter password'
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isInvalid={touched.password && !!errors.password}
+                ></Form.Control>
+                <Button onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </Button>
+              </InputGroup>
+              <Form.Control.Feedback type='invalid'>
+                {errors.password}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group controlId='confirmPassword' className='my-2 my-md-1 '>
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control
-                type='password'
+                type={showPassword ? 'text' : 'password'}
                 placeholder='Confirm password'
-                value={confirmPassword}
-                onChange={(e) => {
-                  setConfirmPassword(e.target.value)
-                }}
+                value={values.confirmPassword}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.confirmPassword && !!errors.confirmPassword}
               ></Form.Control>
+              <Form.Control.Feedback type='invalid'>
+                {errors.confirmPassword}
+              </Form.Control.Feedback>
             </Form.Group>
             <div className='d-grid mt-4 mt-md-3'>
               <Button
+                disabled={isSubmitting}
                 type='submit'
                 variant='secondary'
                 className='w-50 mx-auto'
